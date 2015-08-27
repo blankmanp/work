5c9a8b6448a18a4e2a46b07cf2c1cf1d56017917
blankman.p <blankman.p@gmail.com>
2015-08-27 17:58:49 +0800

    我把后台改成rails了
    php真是世界上最好的语言，臣妾还是用rails吧

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root :to => 'users#index'
+
+  get  '/blog' => 'users#show_blog'
+
+end
---
+++ frontend/home.php
@@ -0,0 +1,21 @@
+<?php 
+	$cssUrl = base_url().'style/css/';
+	$imgUrl = base_url().'style/img/';
+	$jsUrl = base_url().'style/js/';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BlankmanP</title>
+	<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="<?php echo $cssUrl?>bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $cssUrl?>mycss.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $cssUrl?>font-awesome.min.css">
+	<script type="text/javascript" src="<?php echo $jsUrl?>jquery.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
